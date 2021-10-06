70aa3e04d1acdef31ab5ad1897b06e93a5fe6926
Alex <tat1212wong@gmail.com>
2021-10-06 20:32:20 +0800

    CO@ reading and json string generation done

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,106 @@
 #include <Arduino.h>
+#include <SensirionI2CScd4x.h>
+#include <Wire.h>
+
+#define SDA_PIN             21
+#define SCL_PIN             22
+
+SensirionI2CScd4x scd4x;
+
+void printUint16Hex(uint16_t value) {
+    Serial.print(value < 4096 ? "0" : "");
+    Serial.print(value < 256 ? "0" : "");
+    Serial.print(value < 16 ? "0" : "");
+    Serial.print(value, HEX);
+}
+
+void printSerialNumber(uint16_t serial0, uint16_t serial1, uint16_t serial2) {
+    Serial.print("Serial: 0x");
+    printUint16Hex(serial0);
+    printUint16Hex(serial1);
+    printUint16Hex(serial2);
+    Serial.println();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+
+    Serial.begin(9600);
+    while (!Serial) {
+        delay(100);
+    }
+
+    Wire.begin(SDA_PIN, SCL_PIN);
+
+    uint16_t error;
+    char errorMessage[256];
+
+    scd4x.begin(Wire);
+
+    // stop potentially previously started measurement
+    error = scd4x.stopPeriodicMeasurement();
+    if (error) {
+        Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
+
+    uint16_t serial0;
+    uint16_t serial1;
+    uint16_t serial2;
+    error = scd4x.getSerialNumber(serial0, serial1, serial2);
+    if (error) {
+        Serial.print("Error trying to execute getSerialNumber(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        printSerialNumber(serial0, serial1, serial2);
+    }
+
+    // Start Measurement
+    error = scd4x.startPeriodicMeasurement();
+    if (error) {
+        Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
+
+    Serial.println("Waiting for first measurement... (5 sec)");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    uint16_t error;
+    char errorMessage[256];
+
+    delay(5000);
+
+    // Read Measurement
+    uint16_t co2;
+    float temperature;
+    float humidity;
+    error = scd4x.readMeasurement(co2, temperature, humidity);
+    if (error) {
+        Serial.print("Error trying to execute readMeasurement(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else if (co2 == 0) {
+        Serial.println("Invalid sample detected, skipping.");
+    } else {
+        String jsonStr;
+        jsonStr.concat("{\n");
+
+        jsonStr.concat("\"CO2\":");
+        jsonStr.concat(co2);
+        jsonStr.concat(",\n");
+
+        jsonStr.concat("\"Temp\":");
+        jsonStr.concat(temperature);
+        jsonStr.concat(",\n");
+
+        jsonStr.concat("\"Humi\":");
+        jsonStr.concat(humidity);
+
+        jsonStr.concat("\n}");
+
+        Serial.println(jsonStr);
+    }
 }
